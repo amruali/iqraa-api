700f09697133c6a2c88496440d74cd7e19368110
Amr Ali <amrali90111@gmail.com>
2021-11-17 11:39:13 +0200

    Update endpoints.go

--- handlers/endpoints.go
+++ handlers/endpoints.go
@@ -5,12 +5,12 @@ import (
 )
 
 func (s *Server) setupEndpoints(router *chi.Mux) {
+	
+	router.Get("/", func(w http.ResponseWriter, r *http.Request) {
+		json.NewEncoder(w).Encode("Ok")
+	})
+		
 	router.Route("/api", func(router chi.Router) {
-		/*
-			router.Get("/", func(w http.ResponseWriter, r *http.Request) {
-				json.NewEncoder(w).Encode("Ok")
-			})
-		*/
 		router.Route("/users", func(r chi.Router) {
 			//router.Get("/", s.SampleUser)
 			r.Post("/login", s.Login())
