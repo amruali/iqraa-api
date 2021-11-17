d2621b1a76a89e6a41e35cf863a26e1611c3de52
Amr <amrali90111@gmail.com>
2021-11-17 16:36:38 +0200

    :hammer: Update endpoint.js

--- handlers/endpoints.go
+++ handlers/endpoints.go
@@ -6,23 +6,27 @@ import (
 	"net/http"
 )
 
-func (s *Server) setupEndpoints(router *chi.Mux) {
-	
-	router.Get("/favicon.ico", func(w http.ResponseWriter, r *http.Request) {
-		w.Header().Set("Content-Type", "image/x-icon")
-  		w.Header().Set("Cache-Control", "public, max-age=7776000")
-		http.ServeFile(w, r, "../static/favicon.ico")
-	})
-	router.Get("/", func(w http.ResponseWriter, r *http.Request) {
-		json.NewEncoder(w).Encode("Ok")
-	})
-		
-	router.Route("/api", func(router chi.Router) {
-		router.Route("/users", func(r chi.Router) {
-			//router.Get("/", s.SampleUser)
-			r.Post("/login", s.Login())
-			r.Post("/register", s.Register())
-		})
 
+func favHandler(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.WriteHeader(http.StatusNoContent)
+		next.ServeHTTP(w, r)
+	})
+}
+
+func (s *Server) setupEndpoints(router *chi.Mux) {	
+	router.Route("", func(router chi.Router){
+		router.Use(favHandler);
+		router.Get("/", func(w http.ResponseWriter, r *http.Request) {
+			json.NewEncoder(w).Encode("Ok")
+		})
+		router.Route("/api", func(router chi.Router) {
+			router.Route("/users", func(r chi.Router) {
+				//router.Get("/", s.SampleUser)
+				r.Post("/login", s.Login())
+				r.Post("/register", s.Register())
+			})
+	
+		})
 	})
 }
