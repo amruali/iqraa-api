05a35c5064cba42cedbda08a9e70cb8f33f5a0c7
Amr <amrali90111@gmail.com>
2021-11-17 12:30:15 +0200

    :hammer: Updated GET /favicon.ico

--- handlers/endpoints.go
+++ handlers/endpoints.go
@@ -9,6 +9,8 @@ import (
 func (s *Server) setupEndpoints(router *chi.Mux) {
 	
 	router.Get("/favicon.ico", func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Content-Type", "image/x-icon")
+  		w.Header().Set("Cache-Control", "public, max-age=7776000")
 		http.ServeFile(w, r, "../static/favicon.ico")
 	})
 	router.Get("/", func(w http.ResponseWriter, r *http.Request) {
