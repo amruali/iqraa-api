808eea12a9971799b68eeb23c086c0ac9a387220
Amr Ali <dev.amruali@gmail.com>
2024-09-21 02:30:09 +0300

    Update postgres.go

--- postgres/postgres.go
+++ postgres/postgres.go
@@ -2,6 +2,7 @@ package postgres
 
 import (
 	"github.com/go-pg/pg/v9"
+	"github.com/go-pg/pg/v9/orm"
 	_ "github.com/lib/pq"
 )
 
@@ -12,4 +13,34 @@ func ConnectPostgres(DB_URI string) *pg.DB {
 	}
 	DB := pg.Connect(opt)
 	return DB
+}
+
+// Recreate the schema using go-pg ORM
+func recreateSchema(db *pg.DB) error {
+    // Drop tables if they exist
+    models := []interface{}{
+        (*User)(nil),
+        (*Product)(nil),
+    }
+    for _, model := range models {
+        err := db.Model(model).DropTable(&orm.DropTableOptions{
+            IfExists: false,
+            Cascade:  true,
+        })
+        if err != nil {
+            return err
+        }
+    }
+
+    // Create tables based on the model structs
+    for _, model := range models {
+        err := db.Model(model).CreateTable(&orm.CreateTableOptions{
+            Temp: false,
+        })
+        if err != nil {
+            return err
+        }
+    }
+
+    return nil
 }
